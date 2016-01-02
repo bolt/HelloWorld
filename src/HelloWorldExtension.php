263d4c20442b99e1828329b76238f2f45e8ae41b
Gawain Lynch <gawain.lynch@gmail.com>
2016-01-02 10:53:03 +0100

    Update extension class for Bolt v3

--- src/HelloWorldExtension.php
+++ src/HelloWorldExtension.php
@@ -1,37 +1,69 @@
 <?php
-// Hello World Extension for Bolt
-// Minimum version: 0.7
 
 namespace Bolt\Extension\Bolt\HelloWorld;
 
-class Extension extends \Bolt\BaseExtension
+use Bolt\Extension\SimpleExtension;
+
+/**
+ * Class HelloWorldExtension
+ *
+ * @author Bob den Otter <bob@twokings.nl>
+ * @author Xiao-Hu Tai <xiao@twokings.nl>
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class HelloWorldExtension extends SimpleExtension
 {
+    /**
+     * Override the default function to provide a customised name for this extension.
+     *
+     * @return string
+     */
     public function getName()
     {
-        return "Hello World";
+        return 'Hello, World!';
     }
 
-    public function initialize()
+    /**
+     * {@inheritdoc}
+     */
+    protected function registerTwigFunctions()
     {
-        $this->addTwigFunction('helloworld', 'twigHelloworld');
-
+        return [
+            'helloworld' => 'twigHelloworld',
+        ];
     }
 
-    public function twigHelloworld($name="")
+    /**
+     * Our Twig callback function.
+     *
+     * If $name isn't provided, we use the one from the config file, unless
+     * that's empty too, then use "World".
+     *
+     * @param string $name
+     *
+     * @return \Twig_Markup
+     */
+    public function twigHelloworld($name = '')
     {
-        // if $name isn't set, use the one from the config.yml. Unless that's empty too, then use "world".
-        if (empty($name)) {
-            if (!empty($this->config['name'])) {
-                $name = $this->config['name'];
-            } else {
-                $name = "World";
-            }
+        if ($name === '') {
+            $config = $this->getConfig();
+            $name = $config['name'];
         }
 
-        $html = "Hello, ". $name ."!";
+        $html = "Hello, $name!";
 
         return new \Twig_Markup($html, 'UTF-8');
-
     }
 
+    /**
+     * Default config options
+     *
+     * @return array
+     */
+    protected function getDefaultConfig()
+    {
+        return array(
+            'agent' => 'World'
+        );
+    }
 }
